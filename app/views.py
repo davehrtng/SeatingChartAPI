609412c2139f19dc98b9f094aa98fcf8d9f13c95
davehrtng <david.e.harting@gmail.com>
2015-02-06 15:58:07 -0500

    adds create student - /students POST

--- app/views.py
+++ app/views.py
@@ -35,6 +35,21 @@ def get_student(student_id):
 	if len(student) == 0:
 		abort(404)
 	return jsonify({'student':student[0]})
+
+@app.route('/students', methods=['POST'])
+def create_student():
+	#persist a new student
+	if not request.json or 'lastName' not in request.json or 'firstName' not in request.json:
+		abort(400)
+	new_student = {
+		'id':next_student_id(),
+		'lastName':request.json['lastName'],
+		'firstName':request.json['firstName']
+	}
+	students.append(new_student)
+	return make_response(jsonify({'student':new_student}), 201)
+	
+	
 	
 def make_public_student(student):
 	new_student = {}
@@ -44,12 +59,18 @@ def make_public_student(student):
 		else:
 			new_student[field] = student[field]
 	return new_student
+
+def next_student_id():
+	"""Lookup the next available student id"""
+	return max(map(lambda s: s['id'], students)) + 1
 	
 @app.errorhandler(404)
+# TODO: differentiate between bad URL and bad ID
 def not_found(error):
 	return make_response(jsonify({
 		'error': 'Not found',
 		'errorCode':404,
 		'displayableText':'The requested content could not be found'
 		}), 404)
+		
 	
