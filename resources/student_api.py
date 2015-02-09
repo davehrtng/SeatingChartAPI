61ef0357f7b78e91923ccb56e1b26f7483b91ecb
davehrtng <david.e.harting@gmail.com>
2015-02-09 10:27:24 -0500

    small changes to student resource put

--- resources/student_api.py
+++ resources/student_api.py
@@ -21,15 +21,15 @@ students = [
 student_fields = {
 	'lastName':fields.String,
 	'firstName':fields.String,
-	'uri':fields.Url('task')
+	'uri':fields.Url('student')
 }
 
 	
 class StudentApi(Resource):
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
-		self.reqparse.add_argument('lastName', type = str, required = True, help = 'No lastName provided', location = 'json')
-		self.reqparse.add_argument('firstName', type = str, required = True, help = 'No firstName provided', location = 'json')
+		self.reqparse.add_argument('lastName', type=str, required = False, help = 'lastName must be a string', location = 'json')
+		self.reqparse.add_argument('firstName', type=str, required = False, help = 'firstName must be a string', location = 'json')
 		super(StudentApi, self).__init__()
 
 	def get(self, id):
@@ -41,7 +41,8 @@ class StudentApi(Resource):
 			abort(404)
 		student = student[0]
 		args = self.reqparse.parse_args()
-		for k, v in args.iteritems():
+		print(args)
+		for k, v in args.items():
 			if v != None:
 				student[k] = v
 		return {'student': marshal(student, student_fields)}
