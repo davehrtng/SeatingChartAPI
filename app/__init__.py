5630c53decf9f7c00bc5caa56e81e84de9914ff9
davehrtng <david.e.harting@gmail.com>
2015-02-17 10:35:48 -0500

    use CORS to enable ajax calls to server from browser

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask.ext.restful import Api
+from flask.ext.cors import CORS
 from resources.student_api import StudentsApi
 from resources.student_api import StudentsByIdApi
 
 app = Flask(__name__)
+cors = CORS(app)
 api = Api(app)
+
 
 
 api.add_resource(StudentsByIdApi, '/students/<int:id>', endpoint = 'student')
