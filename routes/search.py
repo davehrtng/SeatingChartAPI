491c7e39d4ebfa76b704a7a41ccc9aed190d6f18
davehrtng <david.e.harting@gmail.com>
2015-02-23 11:41:25 -0500

    add lastname search using trie

--- routes/search.py
+++ routes/search.py
@@ -8,9 +8,16 @@ import datrie
 
 uri_base = "/students"
 
+student_trie = datrie.Trie(string.ascii_lowercase)
+load_trie()
+
+def load_trie():
+	student_list = student_collection.get_all()
+	for s in student_list:
+		student_trie[s['lastName']] = s
+
 def make_student_uri(student_id):
 	return uri_base + '/' + str(student_id)
-	
 
 def make_public_student(student):
 	# TODO: should have a uri field instead of id field
@@ -22,6 +29,13 @@ def make_public_student(student):
 			public_student[key] = value
 	return public_student
 
+@app.route('students/search/lastname/<query>', methods=['GET'])
+def student_search_lastname(query):
+	return make_response(jsonify(
+		{
+			'students':[make_public_students(s) for s in student_trie.values(query)]
+		}), 200);
+	
 @app.route('/students/search/<query>', methods=['GET'])
 def student_search(query):
 	query_dict = {}
